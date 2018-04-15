42efa833508bbd9b4377cea3bb900e34b320480d
Julie Groth <groth.julie@gmail.com>
2018-04-14 22:57:23 -0700

    added components and cards.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/nav";
+import Card from "./components/Card";
+import cards from "./cards.json";
+import Game from "./components/game";
 
 class App extends Component {
+
+  state = {
+    cards
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     
+      <Game>
+        {this.state.cards.map(card => (
+          <Card
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+          />
+        ))}
+      </Game>
+      
     );
   }
 }
---
+++ src/components/game.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Game = props => <div className="wrapper">{props.children}</div>;
+
+export default Game;
