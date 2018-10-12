61785e72ca65280242961a2509aa95057fab2473
Julie Groth <groth.julie@gmail.com>
2018-10-11 17:31:46 -0700

    Updates

---
+++ precache-manifest.330f199cd8db220ddd6b518ae9756f6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "0632fd86da5e547ea1ab",
+    "url": "/clicky-game/static/js/main.0632fd86.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "0632fd86da5e547ea1ab",
+    "url": "/clicky-game/static/css/main.d046affa.chunk.css"
+  },
+  {
+    "revision": "538a2f9efab1ecdefc05af57ba6c2c33",
+    "url": "/clicky-game/index.html"
+  }
+];
